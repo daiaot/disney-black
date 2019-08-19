78dbcd3a9baccdc6fff91ea229bcc591f6261e86
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-08-19 16:05:10 +0900

    feat: slider実装

---
+++ src/scripts/modules/Slider.js
@@ -0,0 +1,36 @@
+import $ from 'jquery'
+import 'slick-carousel'
+
+export default class Slider {
+  constructor(selector) {
+    this.$el = $(selector)
+    this.init()
+  }
+
+  init() {
+    this.$el.slick({
+      dots: true,
+      infinite: true,
+      variableWidth: true,
+      autoplay: true,
+      pauseOnFocus: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      speed: 300,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 736,
+          settings: {
+            // slidesToShow: 1,
+            // centerMode: false,
+            // centerPadding: '20px',
+            variableWidth: false,
+            // variableWidth: true,
+          },
+        },
+      ],
+    })
+  }
+}
