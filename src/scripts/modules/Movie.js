74095f2be73901445daebbc8d7e0cdba38da18df
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-08-19 13:56:27 +0900

    feat: kv動画実装

--- src/scripts/modules/Movie.js
+++ src/scripts/modules/Movie.js
@@ -11,49 +11,68 @@ export default class Movie {
 
     console.log('movie - bindEvents()')
 
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-
-    // console.log(firstScriptTag)
-
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    // var tag = document.createElement('script');
+    // tag.src = "https://www.youtube.com/iframe_api";
+    // var firstScriptTag = document.getElementsByTagName('script')[0];
+    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     var player;
-    function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      width: '100%',
+      videoId: 'RTkGg0In1k4',
+      playerVars: {
+        'autoplay': 1,
+        'modestbranding': 1,
+        'rel': 0,
+        'controls': 0,
+        'showinfo': 0,
+        'disablekb': 1,
+        'loop' : 1,
+        'rel': 0,
+        'playlist' : 'RTkGg0In1k4'
+      },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  
 
-      console.log('movie - onYouTubeIframeAPIReady()')
+    // var player;
+    // function onYouTubeIframeAPIReady() {
 
-      player = new YT.Player('player', {
-        height: '360',
-        width: '640',
-        videoId: 'bjmBJ1Fl0cs',
-        playerVars: {
-          controls: 0,
-          autoplay: 1,
-          loop: 1,
-          playlist: 'bjmBJ1Fl0cs',
-          rel: 0,
-          showinfo: 0,
-        },
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
-    }
+    //   console.log('movie - onYouTubeIframeAPIReady()')
 
-    function onPlayerReady(event) {
+    //   player = new YT.Player('player', {
+    //     height: '421.875',
+    //     width: '750',
+    //     videoId: 'bjmBJ1Fl0cs',
+    //     playerVars: {
+    //       'controls': 0,
+    //       'autoplay': 1,
+    //       'loop': 1,
+    //       'playlist': 'bjmBJ1Fl0cs',
+    //       'rel': 0,
+    //       'showinfo': 0
+    //     },
+    //     events: {
+    //       'onReady': onPlayerReady,
+    //       'onStateChange': onPlayerStateChange
+    //     }
+    //   });
+    // }
+
+    function onPlayerReady() {
       console.log('movie - onPlayerReady()')
-      event.target.playVideo();
-    }
+      player.playVideo();
+      player.mute();
 
+    }
 
     var done = false;
-    function onPlayerStateChange(event) {
+    function onPlayerStateChange() {
       console.log('movie - onPlayerStateChange()')
-      if (event.data == YT.PlayerState.PLAYING && !done) {
+      if (player.data == YT.PlayerState.PLAYING && !done) {
         setTimeout(stopVideo, 6000);
         done = true;
       }
