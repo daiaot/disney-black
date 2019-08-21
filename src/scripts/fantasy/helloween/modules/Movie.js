207a931c4c9974180332ae5f841a8c4050f46643
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-08-21 21:35:57 +0900

    feat: レイアウト調整

--- src/scripts/fantasy/helloween/modules/Movie.js
+++ src/scripts/fantasy/helloween/modules/Movie.js
@@ -19,7 +19,7 @@ export default class Movie {
     var player;
     player = new YT.Player('player', {
       width: '100%',
-      videoId: 'RTkGg0In1k4',
+      videoId: 'Shcpf7OcGyY',
       playerVars: {
         'autoplay': 1,
         'modestbranding': 1,
@@ -29,7 +29,7 @@ export default class Movie {
         'disablekb': 1,
         'loop' : 1,
         'rel': 0,
-        'playlist' : 'RTkGg0In1k4'
+        'playlist' : 'Shcpf7OcGyY'
       },
       events: {
         'onReady': onPlayerReady,
