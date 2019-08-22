97aba0f141e975b38ed1966336b3fe8042243106
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-08-22 19:39:21 +0900

    fix: 動画モジュール修正

--- src/scripts/fantasy/halloween2019/blackcoordinate/modules/Movie.js
+++ src/scripts/fantasy/halloween2019/blackcoordinate/modules/Movie.js
@@ -11,10 +11,10 @@ export default class Movie {
 
     console.log('movie - bindEvents()')
 
-    // var tag = document.createElement('script');
-    // tag.src = "https://www.youtube.com/iframe_api";
-    // var firstScriptTag = document.getElementsByTagName('script')[0];
-    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     var player;
     player = new YT.Player('player', {
