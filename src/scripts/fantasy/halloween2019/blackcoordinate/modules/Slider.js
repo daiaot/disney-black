264d63731069b14832470e3c283f498a243a38d1
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-08-27 16:01:56 +0900

    feat: スライダーの吹き出し調整

--- src/scripts/fantasy/halloween2019/blackcoordinate/modules/Slider.js
+++ src/scripts/fantasy/halloween2019/blackcoordinate/modules/Slider.js
@@ -5,22 +5,30 @@ export default class Slider {
   constructor(selector) {
     this.$el = $(selector)
     this.init()
+    this.bindEvents()
   }
 
   init() {
+
+    this.$el.on('init',function(){
+      $('.slide-wrap__balloon').addClass('on');
+      // $('.slide-wrap__balloon').addClass('hide');
+    });
+
     this.$el.slick({
       dots: true,
       infinite: true,
       variableWidth: true,
-      // autoplay: true,
-      autoplay: false,
+      autoplay: true,
       pauseOnFocus: false,
       slidesToShow: 1,
       slidesToScroll: 1,
       infinite: true,
       speed: 300,
       arrows: false,
-      pauseOnHover: true,
+      // pauseOnHover: true,
+      pauseOnHover: false,
+      pauseOnDotsHover: false,
       responsive: [
         {
           breakpoint: 736,
@@ -36,4 +44,22 @@ export default class Slider {
       ],
     })
   }
+
+  bindEvents() {
+    // console.log('Slider - bindEvents()')
+
+    this.$el.on('beforeChange',function(){
+      console.log('Slider - bindEvents() - beforeChange')
+      // $('.slide-wrap__balloon').addClass('hide');
+      $('.slide-wrap__balloon').removeClass('on');
+    });
+    this.$el.on('afterChange',function(){
+      console.log('Slider - bindEvents() - afterChange')
+      $('.slide-wrap__balloon').addClass('on');
+      // $('.slide-wrap__balloon').removeClass('hide');
+    });
+
+  }
+
+
 }
